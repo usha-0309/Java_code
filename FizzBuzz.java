668d44e3e29b71df76d3bf8fb9e5d1bd01bedda8
usha-0309 <ushasahu1999@gmail.com>
2023-05-09 00:02:26 +0530

    add prime no

---
+++ FizzBuzz.java
@@ -0,0 +1,14 @@
+public class FizzBuzz {
+  public static void main (String arg[])
+  {
+    for (int i = 1; i<= 100; i++){
+      if(i%3 ==0){
+        System.out.println("Fizz");
+      } else if (i%5==0){
+            System.out.println("FizzBuzz");
+      }else{
+        System.out.println(i);
+      }
+    }
+  }
+}
